bbce9612f7b1ca1bb2f8fa613c12e8b4b678dd1c
SergeiBoy <s_shchetinin@mail.ru>
2020-06-16 12:10:32 +0500

    Разбили код на модули

---
+++ js/colorize-wizard.js
@@ -0,0 +1,43 @@
+'use strict';
+
+(function () {
+  var pickColor = 1;
+
+  var wizardSetup = document.querySelector('.setup-wizard-appearance');
+  var wizardCoat = wizardSetup.querySelector('.wizard-coat');
+  var wizardCoatInput = wizardSetup.querySelector('input[name="coat-color"]');
+  var wizardEyes = wizardSetup.querySelector('.wizard-eyes');
+  var wizardEyesInput = wizardSetup.querySelector('input[name="eyes-color"]');
+  var fireball = document.querySelector('.setup-fireball-wrap');
+  var fireballInput = fireball.querySelector('input[name="fireball-color"]');
+
+  var onWizardCoatPress = function () {
+    var pickedColor = window.wizardsDescription.COAT_COLORS[pickColor % window.wizardsDescription.COAT_COLORS.length];
+    wizardCoat.style.fill = pickedColor;
+    wizardCoatInput.value = pickedColor;
+    pickColor = (pickColor + 1) % 100;
+  };
+
+  var onWizardEyesPress = function () {
+    var pickedColor = window.wizardsDescription.EYES_COLORS[pickColor % window.wizardsDescription.EYES_COLORS.length];
+    wizardEyes.style.fill = pickedColor;
+    wizardEyesInput.value = pickedColor;
+    pickColor = (pickColor + 1) % 100;
+  };
+
+  var onFireballPress = function () {
+    var pickedColor = window.wizardsDescription.FIREBALL_COLORS[pickColor % window.wizardsDescription.FIREBALL_COLORS.length];
+    fireball.style.backgroundColor = pickedColor;
+    fireballInput.value = pickedColor;
+    pickColor = (pickColor + 1) % 100;
+  };
+
+  window.colorizeWizard = {
+    wizardCoat: wizardCoat,
+    wizardEyes: wizardEyes,
+    fireball: fireball,
+    onWizardCoatPress: onWizardCoatPress,
+    onWizardEyesPress: onWizardEyesPress,
+    onFireballPress: onFireballPress,
+  };
+})();
